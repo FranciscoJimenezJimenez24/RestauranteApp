503b3091872cbfad354bda7ff349edde6e572937
dam2 <franjimenezjimenez24@gmail.com>
2023-10-23 21:24:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/restauranteapp/restaurante/InicioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.restauranteapp.restaurante
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.restauranteapp.R
+
+class InicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
